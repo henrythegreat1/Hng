9a1f57a088c9254c3efdce077a49c4dbf567788c
royalexchangegeneral <royalexchangeplc1@gmail.com>
2022-10-28 18:21:52 +0100

    GET API endpoint

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php 
-echo "<h3>Hello World!</h3>";
+ 
+ header('Access-Control-Allow-Origin: *');
+ header('Access-Control-Request-Method: GET');
+ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Access-Control-Request-Method,Access-Control-Allow-Origin');
+
+ $data = ['slackUsername'=>'Henry Mbamalu','backend'=>true,'age'=>23,'bio'=>'Self-motivated and a problem solver'];
+ echo json_encode($data);
 ?>
