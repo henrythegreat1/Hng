691f6afb1e83e82ac22542a240d2bdfee5ff45bf
royalexchangegeneral <royalexchangeplc1@gmail.com>
2022-10-29 21:09:36 +0100

    content-type

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php 
  
  header('Access-Control-Allow-Origin: *');
+ header('Content-Type: application/json');
  header('Access-Control-Request-Method: GET');
- header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Access-Control-Request-Method,Access-Control-Allow-Origin');
+ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Access-Control-Request-Method,Access-Control-Allow-Origin,Content-Type');
 
  $data = ['slackUsername'=>'manlikehenry','backend'=>true,'age'=>23,'bio'=>'Self-motivated and a problem solver'];
  echo json_encode($data);
