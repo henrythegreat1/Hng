c0b02afe1c63eeb5245fbc9f8093e94163c48d63
royalexchangegeneral <royalexchangeplc1@gmail.com>
2022-11-03 12:25:07 +0100

    reversed style changes

--- fileUpload.php
+++ fileUpload.php
@@ -11,7 +11,7 @@
     box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
     width: 50%;
     margin:50px auto;
-    height: 33vh;
+    height: 30vh;
     display: flex;
     flex-direction: column;
     align-items: center;
